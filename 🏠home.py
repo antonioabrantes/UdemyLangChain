0ef0b91055735bca5c2dc9fdca94a504349aa21e
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-08-25 18:30:27 -0300

    Update 🏠home.py

--- 🏠home.py
+++ 🏠home.py
@@ -52,5 +52,6 @@ title_template = PromptTemplate(
 )
 
 if topico:
-    responsta = llm.invoke(title_template.format(topic=topico,language='english'))
+    resposta = llm.invoke(title_template.format(topic=topico,language='english'))
+    st.write(title_template.format(topic=topico,language='english'))
     st.write(resposta)
