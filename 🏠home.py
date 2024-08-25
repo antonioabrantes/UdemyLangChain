25eac8af4812cf2926a490f5a272ec18f7015fca
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-08-25 17:31:53 -0300

    oi

--- 🏠home.py
+++ 🏠home.py
@@ -1,3 +1,5 @@
+# Udemy Aula 1 - Inicialização no streamlit, githubdesktop e githubdesktop
+
 import streamlit as st
 import os
 from dotenv import load_dotenv
@@ -7,8 +9,21 @@ from dotenv import load_dotenv
 st.title('🏠Início ')
 st.write("Inicialização da chave api...")
 
-# coloque a chave em Setteing /secrets do streamlit.io
+# coloque a chave em Setting /secrets do streamlit.io
 
 load_dotenv()
-api_key = os.getenv("OPENAI_API_KEY")
-st.write(api_key)
+openai_api_key = os.getenv("OPENAI_API_KEY")
+#st.write(openai_api_key)
+
+
+### Udemy Aula 2: acesso a llm
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+
+topico = st.text_input('Entre com sua pergunta')
+
+if topic:
+    llm = ChatOpenAI(temperature=0.0, model="gpt-4o-mini", max_tokens=256, openai_api_key=openai_api_key)
+    resposta = llm.invoke(topico)
+    st.write(resposta)
